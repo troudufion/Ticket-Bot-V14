48e580103ade2ae3de621c285f3ab82580475887
troudufion <mohavsk1@gmail.com>
2025-02-19 23:27:38 +0100

    config.js

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "TOKEN_BOT",
-    ticket_channel: "CHANNEL_ID",
-    ticket_category: "CATEGORY_ID",
+    token: "MTM0MTc5Mzc5MzM1OTM1MTk1MQ.GE05AN.dNt_BCUz3AX7frlMHqSdWa0H9Gsz-4n1ZeHSJk",
+    ticket_channel: "1341898261631471698",
+    ticket_category: "1341898261631471696",
     ticket_logs: "CHANNEL_ID",
     support_team: "ROLE_ID"
 }
